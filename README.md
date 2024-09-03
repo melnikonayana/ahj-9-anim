d033aea2185de8f803159ce88eb9df07ffb43f62
yana <bezotechestvo.yana@gmail.com>
2024-09-03 17:01:12 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+### Статус сборки
+
+[![Build status](https://ci.appveyor.com/api/projects/status/fd5rlt0usiy3fqg8?svg=true)](https://ci.appveyor.com/project/melnikonayana/ahj-4-testing-h47cx)
+
+### Ссылка на GitHub Pages
+
+https://melnikonayana.github.io/ahj-4-testing/
---
+++ src/js/app.js
@@ -0,0 +1,8 @@
+import CollapseWidget from './CollapseWidget.js';
+
+const collapseWidget = new CollapseWidget(
+  document.getElementById('container1'),
+  'Collapse',
+  'Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.',
+);
+collapseWidget.bindToDOM();
